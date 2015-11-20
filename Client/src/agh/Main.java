a38e20686d003c213586437c7a6fee011b7ab13c
Piotr Martyna <piotrek722@gmail.com>
2015-11-20 12:38:13 +0100

    Initial commit

---
+++ Client/src/agh/Main.java
@@ -0,0 +1,13 @@
+package agh;
+
+/**
+ * Created by Peter on 2015-11-20.
+ * Project name : ChatProject
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ Client/src/agh/core/Client.java
@@ -0,0 +1,8 @@
+package agh.core;
+
+/**
+ * Created by Peter on 2015-11-20.
+ * Project name : ChatProject
+ */
+public class Client {
+}

39fa6ddb45e762a67261f32b5adbc7b3d0ec9674
Pretorianin <rafi1994tkd@gmail.com>
2015-11-29 22:33:49 +0100

    Update ServerMain.java

--- Chat/src/agh/ServerMain.java
+++ Chat/src/agh/ServerMain.java
@@ -1,16 +1,12 @@
 package agh;
 
-import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.RemoteException;
-
 /**
  * Created by Peter on 2015-11-21.
  * Project name : ChatProject
  */
 public class ServerMain {
 
-    public static void main(String[] args) throws RemoteException, MalformedURLException {
-    	Naming.rebind("RMIServer", new Server());
+    public static void main(String[] args) {
+        
     }
 }

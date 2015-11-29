5168a6037cfd430fa6db6e236c0c495aacb7f90f
Pretorianin <rafi1994tkd@gmail.com>
2015-11-29 22:33:13 +0100

    Update ClientMain.java

--- Chat/src/agh/ClientMain.java
+++ Chat/src/agh/ClientMain.java
@@ -6,13 +6,14 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
 import agh.ServerInterface;
+
 /**
  * Created by Peter on 2015-11-20.
  * Project name : ChatProject
  */
 public class ClientMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
         String serverURL = "rmi://localhost/RMIServer";
 	ServerInterface server = (ServerInterface) Naming.lookup(serverURL);
 	new Thread(new Client(args[0], server)).start();

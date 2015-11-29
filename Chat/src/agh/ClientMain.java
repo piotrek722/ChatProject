069ca4c0713256934c5ca6e4802fa61e916a075a
Pretorianin <rafi1994tkd@gmail.com>
2015-11-29 22:29:41 +0100

    Update ClientMain.java

--- Chat/src/agh/ClientMain.java
+++ Chat/src/agh/ClientMain.java
@@ -1,6 +1,11 @@
 package agh;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 
+import agh.ServerInterface;
 /**
  * Created by Peter on 2015-11-20.
  * Project name : ChatProject
@@ -8,6 +13,8 @@ package agh;
 public class ClientMain {
 
     public static void main(String[] args) {
-
+        String serverURL = "rmi://localhost/RMIServer";
+		ServerInterface server = (ServerInterface) Naming.lookup(serverURL);
+		new Thread(new Client(args[0], server)).start();
     }
 }

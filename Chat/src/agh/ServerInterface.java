a9ed919c789630a8e77682e001be7894bf4780e1
Pretorianin <rafi1994tkd@gmail.com>
2015-11-30 00:42:27 +0100

    Update ServerInterface.java

--- Chat/src/agh/ServerInterface.java
+++ Chat/src/agh/ServerInterface.java
@@ -8,10 +8,10 @@ import agh.userandmessage.model.Conversation;
 import agh.userandmessage.model.User;
 
 public interface ServerInterface extends Remote {
-	void addContact(User user, String contact) throws RemoteException;
-	void deleteContact(User user, String contact) throws RemoteException;
-	void addUser(User user) throws RemoteException;
-	void deleteUser(User user) throws RemoteException;
+	Boolean addContact(User user, String contact) throws RemoteException;
+	Boolean deleteContact(User user, String contact) throws RemoteException;
+	Boolean addUser(User user) throws RemoteException;
+	Boolean deleteUser(User user) throws RemoteException;
 	ContactList getContacts(User user) throws RemoteException;
 	Conversation getConversations(User user, ContactList contacts) throws RemoteException;
 }

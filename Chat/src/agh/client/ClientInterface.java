fb59bdbfeb91a1a5ceae92c44e1f0ccf21863657
Pretorianin <rafi1994tkd@gmail.com>
2015-12-12 21:34:42 +0100

    Update ClientInterface.java

--- Chat/src/agh/client/ClientInterface.java
+++ Chat/src/agh/client/ClientInterface.java
@@ -3,6 +3,8 @@ package agh.client;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+import agh.userandmessage.model.Message;
+
 public interface ClientInterface extends Remote {
-    void retreiveMessage(String message) throws RemoteException;
+    void retreiveMessage(Message message) throws RemoteException;
 }

a5293a3fb53a74c908a7f2c2caf479fc419598d3
Pretorianin <rafi1994tkd@gmail.com>
2015-12-12 21:35:37 +0100

    Update ServerInterface.java

--- Chat/src/agh/server/ServerInterface.java
+++ Chat/src/agh/server/ServerInterface.java
@@ -2,22 +2,19 @@ package agh.server;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.Date;
 import java.util.List;
 
 import agh.client.ClientInterface;
-import agh.userandmessage.model.Conversation;
 import agh.userandmessage.model.User;
 
 public interface ServerInterface extends Remote {
 	Boolean registerClient(String login, String password, String name, String lastName) throws RemoteException;
 	Boolean unregisterClient(User user) throws RemoteException;
 	User login(ClientInterface client, String login, String password) throws RemoteException;
-	
-	void sendMessage(String message) throws RemoteException;
+	Boolean logout(User user) throws RemoteException;
+	Boolean sendMessage(String content, Date date, User sender, List<String> logins) throws RemoteException;
 	Boolean addContact(User user, String contact) throws RemoteException;
 	Boolean deleteContact(User user, String contact) throws RemoteException;
-	Boolean addUser(User user) throws RemoteException;
-	Boolean deleteUser(User user) throws RemoteException;
 	List<User> getContacts(User user) throws RemoteException;
-	Conversation getConversations(User user, List<User> contacts) throws RemoteException;
 }

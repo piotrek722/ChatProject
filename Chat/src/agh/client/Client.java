66c7835b9f15a5c4e789bda971837f6a4e0102e2
Pretorianin <rafi1994tkd@gmail.com>
2015-12-14 02:36:47 +0100

    Update Client.java

--- Chat/src/agh/client/Client.java
+++ Chat/src/agh/client/Client.java
@@ -6,22 +6,25 @@ import java.rmi.server.UnicastRemoteObject;
 import agh.userandmessage.model.Message;
 import agh.server.ServerInterface;
 
-public class Client extends UnicastRemoteObject implements ClientInterface, Runnable {
+public class Client extends UnicastRemoteObject implements ClientInterface {
 
 	private static final long serialVersionUID = 1L;
 	private ServerInterface server;
 
 	protected Client(ServerInterface server) throws RemoteException {
+		this.setServer(server);
+	}
+
+	@Override
+	public Message retreiveMessage(Message message) throws RemoteException {
+		return message;
+	}
+
+	public ServerInterface getServer() {
+		return server;
+	}
+
+	public void setServer(ServerInterface server) {
 		this.server = server;
-	}
-
-	@Override
-	public void run() {
-		// pass
-	}
-
-	@Override
-	public void retreiveMessage(Message message) throws RemoteException {
-		// TODO Auto-generated method stub
 	}
 }
